3662fd49b00d99353e9003f7d8a0ef9702a9ca57
KimJeonghun91 <suuuk3@naver.com>
2021-03-25 16:22:58 +0900

    작업중...

--- fun/gitPlant.js
+++ fun/gitPlant.js
@@ -1,9 +1,17 @@
+var fs = require('fs');
 var exec = require('child_process').exec;
+
 
 const start = (req, res, next) => {
     console.log("####################################################")
     console.log("################### planting ... ###################")
     console.log("####################################################")
+
+    fs.appendFile('./views/index.jade', `\n  span 🌱`, function (err) {
+        if (err) { return console.log('appendFile error: ' + err); }
+        console.log('The "data to append" was appended to file!');
+    });
+
 
     exec("ls", function (error, stdout, stderr) {
         if (error !== null) { return console.log('exec error: ' + error); }
