7805a56bf8faa900c985f59c000017d8f3bae3e0
KimJeonghun91 <suuuk3@naver.com>
2021-03-27 12:20:56 +0900

    1616815256716

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
-app.use(gitPlant.start) // localhost:3999 에서 즉시 실행
+app.use('/test', gitPlant.start);// localhost:3999/test 에서 즉시 실행
 
 // const task = cron.schedule("*/1 * * * *", () => {       // 1분마다 실행
 const task = cron.schedule("0 9 1-31 * *", () => {   // 1~31일(매일) 9시 0분에 실행
